3835ebfcd1a5ab1540f829812518ef8d8ec1930c
Mario Calise <121984855+Marcal97@users.noreply.github.com>
2023-01-05 00:58:16 +0100

    Add files via upload

---
+++ PythonChallenge.py
@@ -0,0 +1,23 @@
+import numpy as np
+import random
+
+def PythonChallenge():
+  
+  flag = 0
+  list = np.arange(1,10,step=1)
+  
+  while flag == 0:
+     A = random.choice(list)
+     B = random.choice(list)
+     C = A*B
+
+     if C == 4:
+        print('Success! Your values for A and B are: A =', A,'and B =',B)
+        flag = 1
+     else: 
+      print( 'A =',A,'and C =',C)
+       
+  
+  print("Loop is finished! Mission completed :D")
+
+PythonChallenge() 
